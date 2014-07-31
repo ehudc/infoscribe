f4c7c67edb87ac41d9c7d6715b40d78a84790666
ehudc <s.ehud.cohen@gmail.com>
2014-07-31 00:36:52 -0400

    first commit, rails base

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Infoscribe::Application.config.secret_key_base = '5f5cdd82d773590209d47c236d865d40d0e0d6062ff4be5ca8877d0efbf6e9d2a13a430b06301c1d7c0e0305782f7c4a9840e76e593b582dcc2515844085012a'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Infoscribe::Application.config.session_store :encrypted_cookie_store, key: '_infoscribe_session'
